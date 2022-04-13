1522bd94393ce92d24f7a6db3b5b0820d7fe4dcf
KMV <9117479@gmail.com>
2022-04-13 22:39:52 +0300

    NC Lesson24

--- app/src/main/java/com/example/pifagorapp/MainActivity.kt
+++ app/src/main/java/com/example/pifagorapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.pifagorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.eduapplication.constance.Constance
+import com.example.pifagorapp.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun onClickResult(view: View){
+        if (!isFieldEmpty()){
+            val result = getString(R.string.text_result) + getResult()
+            binding.textViewResult.text = result
+        }
+    }
+    private fun isFieldEmpty(): Boolean{
+        binding.apply {
+            if (inputA.text.isNullOrEmpty()) inputA.error = Constance.INPUT_ERROR
+            if (inputB.text.isNullOrEmpty()) inputB.error = Constance.INPUT_ERROR
+            return inputA.text.isNullOrEmpty() || inputB.text.isNullOrEmpty()
+        }
+    }
+    private fun getResult(): String{
+        val a: Double
+        val b: Double
+        binding.apply {
+            a = inputA.text.toString().toDouble()
+            b = inputB.text.toString().toDouble()
+        }
+        return sqrt((a.pow(2) + b.pow(2))).toString()
     }
 }
---
+++ app/src/main/java/constance/Constance.kt
@@ -0,0 +1,7 @@
+package com.example.eduapplication.constance
+
+import android.app.DownloadManager
+
+object Constance {
+    const val INPUT_ERROR = "Empty Field"
+}
